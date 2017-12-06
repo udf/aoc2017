cd8736edf3003e314d1c61065f35456cca3073a4
EyeZiS <tabhooked@gmail.com>
2017-12-06 02:09:27 +0200

    add day 1

---
+++ day1/part1.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+using std::string;
+using std::cin;
+using std::cout;
+
+int main() {
+    string data;
+    cin >> data;
+    
+    size_t length = data.length();
+    int output = 0;
+    for (size_t i = 0; i < length; ++i) {
+        int digit = data[i % length] - '0';
+        int next_digit = data[(i + 1) % length] - '0';
+        
+        if (digit == next_digit) {
+            output += digit;
+        }
+    }
+
+    cout << output;
+
+    return 0;
+}
---
+++ day1/part2.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+using std::string;
+using std::cin;
+using std::cout;
+
+int main() {
+    string data;
+    cin >> data;
+    
+    size_t length = data.length();
+    int sum = 0;
+    for (size_t i = 0; i < length; ++i) {
+        int digit = data[i % length] - '0';
+        int next_digit = data[(i + length/2) % length] - '0';
+        
+        if (digit == next_digit) {
+            sum += digit;
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
